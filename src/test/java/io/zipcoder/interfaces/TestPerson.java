046eec83b7515e68ea5a1e8e2dc7f84a4f112d07
shubham <shubham@zipcodes-MBP-5.fios-router.home>
2020-11-25 22:32:54 -0500

    checking out the lite branch but cannot find the package while running tests

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,20 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+
+    public void testSetName(){
+        String givenName = "Sushim";
+
+        Person person = new Person(givenName);
+        String expectedName = person.getName();
+        Assert.assertEquals(givenName, expectedName);
+    }
+
+
 
 }
